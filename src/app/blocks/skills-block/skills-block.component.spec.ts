e70700bdc3702cbcd2d5d5c40ea89b479f924169
Dum6o <edwardasas@gmail.com>
2022-01-07 17:59:12 +0200

    Update skills tests

--- src/app/blocks/skills-block/skills-block.component.spec.ts
+++ src/app/blocks/skills-block/skills-block.component.spec.ts
@@ -1,11 +1,15 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { of } from 'rxjs';
 import { FirestoreService } from 'src/app/services/firestore.service';
+import { Mock } from 'src/app/testing/mock-data.test';
 import { MockModule } from 'src/app/testing/mock-module.test';
 import { SkillsBlockComponent } from './skills-block.component';
 
 describe('SkillsBlockComponent', () => {
   let component: SkillsBlockComponent;
   let fixture: ComponentFixture<SkillsBlockComponent>;
+  let firestoreService: FirestoreService;
+  let firestoreSpy: jest.SpyInstance;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -25,10 +29,17 @@ describe('SkillsBlockComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(SkillsBlockComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    firestoreService = TestBed.inject(FirestoreService);
+    firestoreSpy = jest.spyOn(firestoreService, 'getCollectionItems');
   });
 
   test('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  test('should get skills on init', () => {
+    firestoreSpy.mockReset().mockReturnValue(of([Mock.skillGroup]));
+    component.ngOnInit();
+    expect(component.skills).toEqual([Mock.skillGroup]);
   });
 });
