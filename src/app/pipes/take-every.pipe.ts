c04d6793ed609b5ac04e5b634601c1a3feef7f95
Dum6o <edwardasas@gmail.com>
2022-01-07 18:48:15 +0200

    Add tests for pipes

--- src/app/pipes/take-every.pipe.ts
+++ src/app/pipes/take-every.pipe.ts
@@ -7,10 +7,12 @@ export class TakeEveryPipe implements PipeTransform {
 
   transform(elements: any[], selector: number = 1, takeFirst = true): any[] {
     let arr = [];
-    if (!takeFirst) {
-      elements.shift();
+    if (elements && elements.length > 0) {
+      if (!takeFirst) {
+        elements.shift();
+      }
+      arr = elements.filter((value, index, array) => index % selector === 0);
     }
-    arr = elements.filter((value, index, array) => index % selector === 0);
     return arr;
   }
 
